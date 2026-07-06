2c091a12090f90d68169eede8f54200aa323f26d
faisfestiawan-maker <fais.festiawan@gmail.com>
2026-07-07 04:42:49 +0700

    Add files via upload

---
+++ Frontend/js/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+
+    API_URL: "https://script.google.com/macros/s/AKfycbyhnxceBu3X7mrUMrSauLHY4XC2kaINJ1vv4eL37H9JX-CIsXxX3YGxai6KRLrpSGiM/exec",
+
+    APP_NAME: "Presensi PKL",
+
+    VERSION: "1.0"
+
+};
